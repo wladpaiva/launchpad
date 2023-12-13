a566f7aebcea7504ff43541cd1a3fc49f2986251
Wlad Paiva <desenvolvedormg@gmail.com>
2023-12-13 18:16:45 -0300

    Add basic form components

---
+++ packages/ui/components/forms.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import {forwardRef} from 'react'
+import {useFormStatus} from 'react-dom'
+
+import {Spinner} from './icons'
+import {Button, ButtonProps} from './ui/button'
+
+type SubmitButtonProps = Omit<ButtonProps, 'type' | 'aria-disabled'>
+
+const SubmitButton = forwardRef<HTMLButtonElement, SubmitButtonProps>(
+  ({children, ...rest}, ref) => {
+    const {pending} = useFormStatus()
+
+    return (
+      <Button {...rest} type="submit" aria-disabled={pending} ref={ref}>
+        {pending && <Spinner className="mr-2 h-4 w-4 animate-spin" />}
+        {children}
+      </Button>
+    )
+  },
+)
+SubmitButton.displayName = 'SubmitButton'
+
+export {SubmitButton}
