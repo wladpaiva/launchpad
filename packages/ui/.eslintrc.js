ec3c9f979fc5d7c932ba0658a838da96a55f308d
Wlad Paiva <desenvolvedormg@gmail.com>
2023-12-13 15:57:07 -0300

    WIP solve the eslint error on vscode

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
   extends: ['@repo/eslint-config/react-internal.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.lint.json',
+    // this looks important but
+    // https://discord.com/channels/818588653005176832/818588653311754243/1184568983681257592
+    // project: './tsconfig.lint.json',
   },
 }
