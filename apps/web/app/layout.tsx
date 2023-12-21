ce9ca656b088e6e6bb5544c10b0b256870d84c84
Wlad Paiva <desenvolvedormg@gmail.com>
2023-12-21 16:13:34 -0300

    Add Analytics and Page Speed tools

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,6 @@
 import {cn} from '@repo/design-system/lib/utils'
+import {Analytics} from '@vercel/analytics/react'
+import {SpeedInsights} from '@vercel/speed-insights/next'
 import type {Metadata} from 'next'
 import {Inter as FontSans} from 'next/font/google'
 
@@ -40,6 +42,8 @@ export default function RootLayout({
           {children}
           <TailwindIndicator />
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
