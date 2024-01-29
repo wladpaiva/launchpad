51a1dcac16f8628af4383526ce63ab0b57626600
Wlad Paiva <desenvolvedormg@gmail.com>
2024-01-29 17:59:39 -0300

    feat: add hero

--- apps/web/app/(marketing)/page.tsx
+++ apps/web/app/(marketing)/page.tsx
@@ -1,10 +1,13 @@
 export default function Page() {
   return (
-    <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 my-24 sm:my-32">
-      Edit me at{' '}
-      <span className="rounded bg-muted px-1.5">
-        apps/web/app/(marketing)/page.tsx
-      </span>
-    </div>
+    <>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 my-24 sm:my-32">
+        Edit me at{' '}
+        <code className="font-mono rounded bg-muted px-1.5">
+          apps/web/app/(marketing)/page.tsx
+        </code>
+      </div>
+      {/* <Hero /> */}
+    </>
   )
 }
