f10fbdc1966960e4761ffc74b2f56918dcce0cc8
Wlad Paiva <desenvolvedormg@gmail.com>
2024-01-30 11:27:22 -0300

    feat: features-grid

---
+++ apps/web/components/sections/features-grid.tsx
@@ -0,0 +1,78 @@
+import {ServerIcon} from '@repo/design-system/components/icons'
+
+const features = [
+  {
+    name: 'Push to deploy.',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+    icon: ServerIcon,
+  },
+  {
+    name: 'SSL certificates.',
+    description:
+      'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: ServerIcon,
+  },
+  {
+    name: 'Simple queues.',
+    description:
+      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.',
+    icon: ServerIcon,
+  },
+  {
+    name: 'Advanced security.',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+    icon: ServerIcon,
+  },
+  {
+    name: 'Powerful API.',
+    description:
+      'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: ServerIcon,
+  },
+  {
+    name: 'Database backups.',
+    description:
+      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. ',
+    icon: ServerIcon,
+  },
+]
+
+export function FeaturesGrid() {
+  return (
+    <div className="bg-muted/50 py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl sm:text-center">
+          <h2 className="text-base font-semibold leading-7 text-primary">
+            Everything you need
+          </h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+            No server? No problem.
+          </p>
+          <p className="mt-6 text-lg leading-8 text-muted-foreground">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+            impedit perferendis suscipit eaque, iste dolor cupiditate
+            blanditiis.
+          </p>
+        </div>
+      </div>
+      <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+        <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-muted-foreground sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+          {features.map(feature => (
+            <div key={feature.name} className="relative pl-9">
+              <dt className="inline font-semibold">
+                <feature.icon
+                  className="absolute left-1 top-1 h-5 w-5 text-primary"
+                  aria-hidden="true"
+                />
+                {feature.name}
+              </dt>{' '}
+              <dd className="inline">{feature.description}</dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </div>
+  )
+}
