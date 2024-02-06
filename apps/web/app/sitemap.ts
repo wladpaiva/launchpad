7151b871429c1eb648c39cba07cb6368fbf60445
Wlad Paiva <desenvolvedormg@gmail.com>
2024-02-06 12:48:40 -0300

    fix: add pages to sitemap

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -1,7 +1,7 @@
 import {BASE_URL} from '@/lib/env.server'
 
 export default async function sitemap() {
-  let routes = [''].map(route => ({
+  let routes = ['/', '/signin', '/signup'].map(route => ({
     url: `${BASE_URL}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
