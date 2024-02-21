08d7cf4eb61155f8f0a4cac4f41cc9cc49e7c176
Wlad Paiva <desenvolvedormg@gmail.com>
2024-02-21 19:19:47 -0300

    Add sentry to web app

---
+++ apps/web/app/global-error.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import {useEffect} from 'react'
+import * as Sentry from '@sentry/nextjs'
+import Error from 'next/error'
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & {digest?: string}
+  reset: () => void
+}) {
+  useEffect(() => {
+    Sentry.captureException(error)
+  }, [error])
+
+  return (
+    <html>
+      <body>
+        <Error statusCode={error.props.statusCode} />
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  )
+}
