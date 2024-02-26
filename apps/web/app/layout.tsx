271366ec6b8478f6a1ade64dccd4fed45546cf40
Wlad Paiva <desenvolvedormg@gmail.com>
2024-02-26 14:50:01 -0300

    fix: use dynamic height on layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className="scrollbar-stable">
       <body
         className={cn(
-          'min-h-screen bg-background font-sans antialiased',
+          'min-h-dvh bg-background font-sans antialiased',
           fontSans.variable,
         )}
       >
