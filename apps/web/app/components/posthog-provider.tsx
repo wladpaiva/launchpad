2322ccdda2f770341a0fdaab85d73c133af7fba8
Wlad Paiva <desenvolvedormg@gmail.com>
2024-02-27 10:40:14 -0300

    Add displayName to PostHogProviderBase

--- apps/web/app/components/posthog-provider.tsx
+++ apps/web/app/components/posthog-provider.tsx
@@ -7,3 +7,6 @@ import {PostHogProvider as PostHogProviderBase} from 'posthog-js/react'
 export function PostHogProvider({children}: {children: React.ReactNode}) {
   return <PostHogProviderBase client={posthog}>{children}</PostHogProviderBase>
 }
+
+// @ts-ignore - displayName is a React static property
+PostHogProviderBase.displayName = 'PostHogProviderBase'
