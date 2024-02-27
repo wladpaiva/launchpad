60a584f4ce7511db1b59ecebf17be4d1d7b885e8
Wlad Paiva <desenvolvedormg@gmail.com>
2024-02-27 15:47:06 -0300

    wip: sitemap

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -1,16 +1,26 @@
 import {NEXT_PUBLIC_URL} from '@/lib/env.server'
+import {db} from '@repo/db'
 
 export default async function sitemap() {
-  let routes = [
+  let staticRoutes = [
     '/',
-    '/signin',
-    '/signup',
-    '/privacy-policy',
-    '/terms-of-service',
+    // '/signin',
+    // '/signup',
+    // '/privacy-policy',
+    // '/terms-of-service',
   ].map(route => ({
     url: `${NEXT_PUBLIC_URL}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
-  return [...routes]
+  const waitlists = await db.query.waitlist.findMany()
+
+  const dynamicRoutes = [
+    ...waitlists.map(waitlist => ({
+      url: `${NEXT_PUBLIC_URL}/${waitlist.slug}`,
+      lastModified: new Date().toISOString().split('T')[0],
+    })),
+  ]
+
+  return [...staticRoutes, ...dynamicRoutes]
 }
