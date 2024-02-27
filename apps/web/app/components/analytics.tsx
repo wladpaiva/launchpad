ee529ee7ac5ca09ef498813700bb20610a744632
Wlad Paiva <desenvolvedormg@gmail.com>
2024-02-27 10:31:29 -0300

    refactor: remove unused analytics code

--- apps/web/app/components/analytics.tsx
+++ apps/web/app/components/analytics.tsx
@@ -3,16 +3,12 @@
 
 import {useEffect} from 'react'
 import analytics from '@/lib/analytics'
-import {usePathname, useSearchParams} from 'next/navigation'
 
 export function Analytics() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
-  // Track pageviews
   useEffect(() => {
+    // Track pageviews
     analytics.page()
-  }, [pathname, searchParams])
+  }, [])
 
   return <></>
 }
