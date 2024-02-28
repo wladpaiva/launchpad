ebc8665c1cbb4eccab37fa27fdb770e9256ab3c2
Wlad Paiva <desenvolvedormg@gmail.com>
2024-02-28 09:03:52 -0300

    disable custom

--- apps/web/app/[slug]/custom-pixel.tsx
+++ apps/web/app/[slug]/custom-pixel.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import React, {useEffect} from 'react'
-import {setCustomPixelId} from '@/lib/analytics-facebook'
+
+// import {setCustomPixelId} from '@/lib/analytics-facebook'
 
 export function CustomPixel({id}: {id: string}) {
   useEffect(() => {
-    setCustomPixelId(id)
+    // setCustomPixelId(id)
   }, [id])
 
   return <></>
